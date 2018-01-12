c051634a8abb8445efaf48018b69246827e8ddf7
鳥居健太郎 <kentaro@MacBook-Air.local>
2018-01-12 22:51:54 +0900

    init

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+1.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = Faker::Name.name
+  profile = Faker::University.name
+
+  User.create!(email: 'couns1@tmp.com',
+               password: password,
+               password_confirmation: password,
+               name: name,
+               confirmed_at: Time.now,
+               confirmation_sent_at: Time.now,
+               type: 'Consultant',
+               user_type: 'Consultant',
+               profile: profile
+               )
+end
 
 9.times do |n|
   email = Faker::Internet.email
@@ -22,6 +39,22 @@
                type: 'Consultant',
                user_type: 'Consultant',
                profile: profile
+               )
+end
+
+
+1.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = Faker::Name.name
+  User.create!(email: 'client1@tmp.com',
+               password: password,
+               password_confirmation: password,
+               name: name,
+               confirmed_at: Time.now,
+               confirmation_sent_at: Time.now,
+               type: 'Client',
+               user_type: 'Client'
                )
 end
 
