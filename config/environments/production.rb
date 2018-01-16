691edbb3500bf96e08158c073ded0c4cec6e2e5d
鳥居健太郎 <kentaro@MacBook-Air.local>
2018-01-16 22:44:55 +0900

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'ancient-badlands-48486.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'counselink.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
